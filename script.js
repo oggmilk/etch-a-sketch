e3c62d8eec214ab2dffda4cedf97139589834ed1
oggmilk <mattdragicevic@gmail.com>
2022-09-28 21:38:47 -0700

    Creates input button that clears/resize grid

--- script.js
+++ script.js
@@ -1,19 +1,30 @@
-let n = 5;
+function createGrid(numberOfSquares){
+  for (let i = 0; i < numberOfSquares; i++) {
+    const container = document.querySelector("#container");
+    const row = document.createElement("div");
+    row.classList.add("row");
+    container.appendChild(row);
+    for (let i = 0; i < numberOfSquares; i++) {
+      const col = document.createElement("div");
+      col.classList.add("col");
+      row.appendChild(col);
+    } //creates an x by x grid
+  }
+  const square = document.querySelectorAll(".col");
+  square.forEach(event => {
+    event.addEventListener("mouseover", () =>{
+      event.classList.add("filled");
+    })}); //mouse-over adds fill in class to cols
 
-for (let i = 0; i < n; i++) {
-  const container = document.querySelector("#container");
-  const row = document.createElement("div");
-  row.classList.add("row");
-  container.appendChild(row);
-  for (let i = 0; i < n; i++) {
-    const col = document.createElement("div");
-    col.classList.add("col");
-    row.appendChild(col);
-  } //creates an n by n grid
 }
 
-const square = document.querySelectorAll(".col");
-square.forEach(event => {
-  event.addEventListener("mouseover", () =>{
-    event.classList.add("filled");
-  })}); //mouse-over adds fill in class to cols
+function newGrid(){
+  let numberOfSquares = prompt("enter:");
+  const removeRow = document.querySelectorAll(".row");
+  removeRow.forEach(row => {
+    row.remove();
+  });
+  createGrid(numberOfSquares);
+} //erases old grid and creates new grid sized off user input
+
+createGrid(16); //default grid
