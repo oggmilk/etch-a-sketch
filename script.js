c8ddeaf5fca13bdc54d8a6f4e2db181ee96b10f6
oggmilk <mattdragicevic@gmail.com>
2022-09-28 18:57:38 -0700

    Creates grid of square divs

---
+++ script.js
@@ -0,0 +1,13 @@
+let n = 100;
+
+for (let i = 0; i < n; i++) {
+  const container = document.querySelector("#container");
+  const row = document.createElement("div");
+  row.classList.add("row");
+  container.appendChild(row);
+  for (let i = 0; i < n; i++) {
+    const col = document.createElement("div");
+    col.classList.add("col");
+    row.appendChild(col);
+  } //creates and n by n grid
+}
